8fe674e6ef6e6b2a5328d6d8cef498f9a3d6bec2
Sanchi Agarwal <agarwalsanchi4@gmail.com>
2017-01-26 23:49:25 +0530

    Add files via upload

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Jarvis = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Jarvis
+            // 
+            this.Jarvis.AutoSize = true;
+            this.Jarvis.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Jarvis.Font = new System.Drawing.Font("MS Reference Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Jarvis.ForeColor = System.Drawing.Color.Lime;
+            this.Jarvis.Location = new System.Drawing.Point(86, 28);
+            this.Jarvis.Name = "Jarvis";
+            this.Jarvis.Size = new System.Drawing.Size(98, 35);
+            this.Jarvis.TabIndex = 0;
+            this.Jarvis.Text = "Jarvis";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(37, 80);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(200, 145);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Jarvis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Jarvis;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine speechreco = new SpeechRecognitionEngine();
+        SpeechSynthesizer jarvis = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+            speechreco.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(speechreco_SpeechRecognized);
+            LoadGrammar();
+            speechreco.SetInputToDefaultAudioDevice();
+            speechreco.RecognizeAsync(RecognizeMode.Multiple);
+        }
+        private void LoadGrammar()
+        {
+            Choices texts = new Choices();
+            string[] lines = File.ReadAllLines(Environment.CurrentDirectory+"\\commands.txt");
+            texts.Add(lines);
+            Grammar wordsList = new Grammar(new GrammarBuilder(texts));
+            speechreco.LoadGrammar(wordsList);
+        }
+        private void speechreco_SpeechRecognized(object sender,SpeechRecognizedEventArgs e)
+        {
+            richTextBox1.Text = e.Result.Text;
+            string speech = e.Result.Text;
+            if(speech == "hello")
+            {
+                jarvis.Speak("hello and good morning");
+            }
+            if(speech=="open bing")
+            {
+                jarvis.Speak("loading");
+                System.Diagnostics.Process.Start("http://www.bing.com");
+            }
+            if (speech == "Who am I")
+            {
+                jarvis.Speak("Your name is Sanchi ");
+            }
+            /*else
+            {
+                jarvis.Speak("I didn't catch that ");
+            }*/
+        }
+    }
+}
